b2fd93299757ef3f24c8203c58408f144aaabdac
Kim <Kim@LAPTOP-GJ3TKL2F>
2024-03-26 21:12:31 +0200

    Add project files.

---
+++ BlazorAppKim/Data/GlobalSettings.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Components;
+using Model;
+
+namespace BlazorAppKim.Data
+{
+    public class GlobalSettings
+    {
+        public static Person login_user;
+        public static NavigationManager nav;
+    }
+}
---
+++ BlazorAppKim/Program.cs
@@ -0,0 +1,38 @@
+using BlazorAppKim.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorAppKim
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+            builder.Services.AddScoped<Trip_4U_ApiService.ApiService >();
+            builder.Services.AddSingleton<Trip_4U_ApiService.ApiService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.MapBlazorHub();
+            app.MapFallbackToPage("/_Host");
+
+            app.Run();
+        }
+    }
+}
